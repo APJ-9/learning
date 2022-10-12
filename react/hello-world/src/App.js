c90c0e3a14c59a1c3fa71a2a0018bce48ff523e4
APJ-9 <akhil.jose@vonnue.com>
2022-10-12 11:45:18 +0530

    methods as props

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -9,6 +9,7 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
@@ -25,7 +26,8 @@ function App() {
       {/* <Counter></Counter> */}
       {/* <FunctionClick></FunctionClick> */}
       {/* <ClassClick></ClassClick> */}
-      <EventBind></EventBind>
+      {/* <EventBind></EventBind> */}
+      <ParentComponent></ParentComponent>
     </div>
   );
 }
