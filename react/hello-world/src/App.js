62c983c4164fd3ecd343de17f9f1daec20c0664d
APJ-9 <akhil.jose@vonnue.com>
2022-10-12 11:09:26 +0530

    event handeling

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -6,6 +6,8 @@ import Hello from './components/Hello';
 import GreetProp from './components/GreetProp';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
@@ -13,13 +15,15 @@ function App() {
       {/* <Greet /> */}
       {/* <Welcome name="Akhil" shortHand="APJ" /> */}
       {/* <Hello /> */}
-      <GreetProp name="Akhil" />
+      {/* <GreetProp name="Akhil" /> */}
       {/* <button>Hit Me</button> */}
       {/* </GreetProp> */}
-      {/* <GreetProp name="APJ" /> */}
+      <GreetProp name="APJ" />
 
       {/* <Message></Message> */}
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      {/* <FunctionClick></FunctionClick> */}
+      <ClassClick></ClassClick>
     </div>
   );
 }
