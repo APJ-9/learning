a17039251b310f9515d5acebea18e96b21ddbd86
APJ-9 <akhil.jose@vonnue.com>
2022-10-13 16:57:10 +0530

    Reducer Hook

--- react/effect-hook/src/App.js
+++ react/effect-hook/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HookCounterOne from './components/HookCounterOne';
@@ -6,11 +6,21 @@ import HookMouse from './components/HookMouse';
 import MouseContainer from './components/MouseContainer';
 import IntervelHookCounter from './components/IntervelHookCounter';
 import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC';
+
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'Akhil'}>
+        <ChannelContext.Provider value={'APJ'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+      {/* <DataFetching /> */}
       {/* <IntervelHookCounter /> */}
       {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
--- react/reducer-hook/src/App.js
+++ react/reducer-hook/src/App.js
@@ -1,24 +1,45 @@
+import React, { useReducer, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
+
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
+    // <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Count - {count} */}
+      {/* <ComponentA /> */}
+      {/* <ComponentB /> */}
+      {/* <ComponentC /> */}
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterThree /> */}
     </div>
+    // </CountContext.Provider>
   );
 }
 
