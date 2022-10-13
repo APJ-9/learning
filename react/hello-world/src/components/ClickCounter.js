22107ab2538b43e848bd2ae36fb2000a1088c078
APJ-9 <akhil.jose@vonnue.com>
2022-10-13 08:53:59 +0530

    HOC

--- react/hello-world/src/components/ClickCounter.js
+++ react/hello-world/src/components/ClickCounter.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react'
+import UpdatedComponent from './withCounter'
 
 class ClickCounter extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            count: 0
-        }
-    }
-
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1 }
-        })
-    }
     render() {
         const { count } = this.state
         return (
@@ -24,4 +13,4 @@ class ClickCounter extends Component {
     }
 }
 
-export default ClickCounter
+export default UpdatedComponent(ClickCounter)
