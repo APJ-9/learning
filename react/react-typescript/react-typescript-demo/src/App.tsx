5348d7f128cfaa70be8339725eb3d15f1a7c2e37
APJ-9 <akhil.jose@vonnue.com>
2022-10-14 15:23:40 +0530

    basic props

---
+++ react/react-typescript/react-typescript-demo/src/App.tsx
@@ -0,0 +1,39 @@
+import './App.css';
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+
+
+function App() {
+  const personName = {
+    first:'Bruce',
+    last:'Wayne'
+  }
+
+  const nameList = [
+    {
+      first:'Bruce',
+      last:'Wayne'
+    },
+    {
+      first:'Clark',
+      last:'Kent'
+    },
+    {
+      first:'Princess',
+      last:'Diana'
+    }
+  ]
+
+  return (
+    <div className="App">
+      <Greet name="APJ" messageCount={10} isLoggedIn={true}></Greet>
+      <Person name={personName}/>
+      <PersonList names={nameList}/>
+      <Status status='error'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-typescript/react-typescript-demo/src/components/Person.tsx
@@ -0,0 +1,9 @@
+type PersonProp ={
+    name: {
+        first: string;
+        last: string;
+    }
+}
+export const Person =(props:PersonProp)=>{
+    return <div>{props.name.first} {props.name.last}</div>
+}
