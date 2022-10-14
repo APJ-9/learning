b7b66b82115aa87d43a08240d23bfdc638b3d376
APJ-9 <akhil.jose@vonnue.com>
2022-10-14 11:53:06 +0530

    Same Element Reference

--- react/render-demo/src/App.js
+++ react/render-demo/src/App.js
@@ -4,6 +4,10 @@ import { UseState } from './components/UseState/UseState';
 import { UseReducer } from './components/UseReducer/UseReducer';
 import { ObjectUseState } from './components/ImmutableState/ObjectUseState';
 import { ArrayUseState } from './components/ImmutableState/ArrayUseState';
+import { Parent } from './components/ParentChild/Parent';
+import { ParentOne } from './components/Optimization/ParentOne';
+import { ChildOne } from './components/Optimization/ChildOne';
+import { GrandParent } from './components/Optimization/GrandParent';
 
 function App() {
   return (
@@ -11,7 +15,9 @@ function App() {
       {/* <UseState /> */}
       {/* <UseReducer /> */}
       {/* <ObjectUseState /> */}
-      <ArrayUseState />
+      {/* <ArrayUseState /> */}
+      {/* <Parent /> */}
+      <GrandParent />
     </div>
   );
 }
