a8d99ef495052d7572b35190b87c2a7c156c7ad7
APJ-9 <akhil.jose@vonnue.com>
2022-10-17 10:29:52 +0530

    completed the header top

---
+++ websites/airbnb/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+import HeaderTop from './HeaderTop'
+function Header() {
+    return (
+        <div className='header'>
+            <HeaderTop />
+        </div>
+    )
+}
+
+export default Header
