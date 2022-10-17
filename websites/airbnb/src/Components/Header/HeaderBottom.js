39fc9dd1218fff3457016c5a2075dde61e742095
APJ-9 <akhil.jose@vonnue.com>
2022-10-17 19:18:28 +0530

    completed the scroller click effect

--- websites/airbnb/src/Components/Header/HeaderBottom.js
+++ websites/airbnb/src/Components/Header/HeaderBottom.js
@@ -1,30 +1,29 @@
-import React,{useRef} from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import sliderCardData from '../../data.json'
 
 
 function HeaderBottom() {
-    const cards =  sliderCardData.header
+    const cards = sliderCardData.header
     console.log(sliderCardData.header[0])
-    const ref = useRef(null)
-    const activeClass = () =>{
-        const cardRef = ref.current
-       console.log(cardRef)
+    function clickHandler(e) {
+        console.log(e.currentTarget)
+        // e.currentTarget.classList.add('active')
     }
     return (
         <div className='header-bottom'>
             <div className="header-slider">
                 <div className="header-slider-inner">
-                        {
-                            cards.map((card,index)=>
-                                <div key={index} ref= {ref} onClick={activeClass} className={index===0?"element-card first active":"element-card"}>
-                                     <div className="img">
-                                        <img src={card.imageURL} alt="url broken" />
-                                     </div>
-                                     <p>{card.title}</p>
+                    {
+                        cards.map((card, index) =>
+                            <button key={index} className={index === 0 ? "element-card first" : "element-card"} onClick={clickHandler}>
+                                <div className="img">
+                                    <img src={card.imageURL} alt="url broken" />
                                 </div>
-                            )
-                        }
+                                <p>{card.title}</p>
+                            </button>
+                        )
+                    }
                 </div>
             </div>
         </div>
